2a41bc07515d4433d98581bf7cea044b0bb5235e
Katie <maugorbit@gmail.com>
2024-09-19 21:15:16 -0400

    Added a basic main file and a gitignore.

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+///Serves as an entry point for the editor. Naturally, not all operations
+///will be executed in this file.
+int main(int argc, char **argv) {
+    printf("%s", "Hello!!! \n");
+    return 0;
+}
